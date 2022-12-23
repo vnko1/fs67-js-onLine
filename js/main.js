63fe566c602b10460f22e4ff7854ea85a195dc96
Andrey Valenko <andreyvalenko@gmail.com>
2022-12-23 10:50:35 +0200

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,109 @@
+const cars = [
+  {
+    make: 'Honda',
+    model: 'CR-V',
+    type: 'suv',
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  },
+  {
+    make: 'Honda',
+    model: 'Accord',
+    type: 'sedan',
+    amount: 2,
+    price: 22455,
+    onSale: true,
+  },
+  {
+    make: 'Mazda',
+    model: 'Mazda 6',
+    type: 'sedan',
+    amount: 8,
+    price: 24195,
+    onSale: false,
+  },
+  {
+    make: 'Mazda',
+    model: 'CX-9',
+    type: 'suv',
+    amount: 7,
+    price: 31520,
+    onSale: true,
+  },
+  {
+    make: 'Toyota',
+    model: '4Runner',
+    type: 'suv',
+    amount: 19,
+    price: 34210,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Sequoia',
+    type: 'suv',
+    amount: 16,
+    price: 45560,
+    onSale: false,
+  },
+  {
+    make: 'Toyota',
+    model: 'Tacoma',
+    type: 'truck',
+    amount: 4,
+    price: 24320,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'F-150',
+    type: 'truck',
+    amount: 11,
+    price: 27110,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Fusion',
+    type: 'sedan',
+    amount: 13,
+    price: 22120,
+    onSale: true,
+  },
+  {
+    make: 'Ford',
+    model: 'Explorer',
+    type: 'suv',
+    amount: 6,
+    price: 31660,
+    onSale: false,
+  },
+];
+
+// const sortByAscendingAmount = cars =>
+//   [...cars].sort((a, b) => a.amount - b.amount);
+
+// console.table(cars);
+// console.table(sortByAscendingAmount(cars));
+
+// ------------------------
+
+// Example 10 - Метод reduce
+// Нехай функція getTotalAmount повертає загальну кількість автомобілів (значення властивостей amount).
+
+// const getTotalAmount = cars =>
+//   cars.reduce((total, car) => total + car.amount, 0);
+
+// console.log(getTotalAmount(cars));
+
+// Example 12 - Ланцюжки методів
+// Нехай функція getSortedCarsOnSale повертає масив автомобілів на розпродажі (Властивість onSale), відсортованих за зростанням ціни.
+
+const getSortedCarsOnSale = cars =>
+  cars.filter(car => car.onSale).sort((a, b) => a.price - b.price);
+
+console.table(getSortedCarsOnSale(cars));
+
+// cars
+console.table('🚀 ~ file: main.js:109 ~ cars', cars);
